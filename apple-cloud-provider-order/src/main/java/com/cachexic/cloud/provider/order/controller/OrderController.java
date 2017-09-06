60bb73836bf46ab7625abdc3f1dd67737c5ef993
tangmin <191102902@qq.com>
2017-09-06 18:18:05 +0800

    feign层集成

---
+++ apple-cloud-provider-order/src/main/java/com/cachexic/cloud/provider/order/controller/OrderController.java
@@ -0,0 +1,40 @@
+package com.cachexic.cloud.provider.order.controller;
+
+import com.cachexic.cloud.common.base.Result;
+import com.cachexic.cloud.common.utils.json.JsonUtil;
+import com.cachexic.cloud.feign.order.entity.Order;
+import com.cachexic.cloud.feign.order.entity.query.OrderQuery;
+import com.cachexic.cloud.feign.order.feign.OrderFeign;
+import com.cachexic.cloud.provider.order.dao.OrderDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author tangmin
+ * @version V1.0
+ * @Title: OrderController.java
+ * @Package com.cachexic.cloud.provider.order.controller
+ * @Description: 
+ * @date 2017-09-06 18:13:33
+ */
+@RestController
+@RequestMapping("/order")
+public class OrderController implements OrderFeign{
+    private final static Logger log = LoggerFactory.getLogger(OrderController.class);
+
+    @Autowired
+    private OrderDao orderDao;
+
+    @Override
+    public Result<List<Order>> selectList(@RequestBody OrderQuery orderQuery) {
+        log.debug(JsonUtil.toJson(orderQuery));
+        List<Order> orders = orderDao.selectList();
+        return Result.OK().setData(orders);
+    }
+}
