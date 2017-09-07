9b1b86c7b3537da5ce59e7e8d5216b9f4cfa9bce
tangmin <191102902@qq.com>
2017-09-07 15:02:52 +0800

    禁用掉db的健康检查，因为用的sharding-jdbc

--- apple-cloud-config-provider/src/main/java/com/cachexic/cloud/config/sjdbc/DbHealthIndicator.java
+++ apple-cloud-config-provider/src/main/java/com/cachexic/cloud/config/sjdbc/DbHealthIndicator.java
@@ -1,4 +1,4 @@
-package com.cachexic.cloud.provider.config;
+package com.cachexic.cloud.config.sjdbc;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
