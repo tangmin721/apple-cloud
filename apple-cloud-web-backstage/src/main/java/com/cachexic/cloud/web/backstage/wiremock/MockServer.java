686786ffe5ba2622d3dd6fb847afdb70154b7704
tangmin721 <191102902@qq.com>
2017-09-24 17:34:58 +0800

    wiremock

--- apple-cloud-web-backstage/src/main/java/com/cachexic/cloud/web/backstage/wiremock/MockServer.java
+++ apple-cloud-web-backstage/src/main/java/com/cachexic/cloud/web/backstage/wiremock/MockServer.java
@@ -1,6 +1,3 @@
-/**
- *
- */
 package com.cachexic.cloud.web.backstage.wiremock;
 
 import java.io.IOException;
@@ -11,7 +8,6 @@ import org.springframework.core.io.ClassPathResource;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.removeAllMappings;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 
@@ -25,17 +21,15 @@ public class MockServer {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        configureFor(9999);
-        removeAllMappings();
+        configureFor("localhost", 9999);
 
-        String urlPre = "http://localhost";
-
-        mock(urlPre + "/order/1", "01");
-        mock(urlPre + "/order/2", "02");
+        mock("/order/1", "01");
+        mock("/order/2", "02");
     }
 
     private static void mock(String url, String file) throws IOException {
-        ClassPathResource resource = new ClassPathResource("mock/response/" + file + ".txt");
+
+        ClassPathResource resource = new ClassPathResource("mock/order/" + file + ".json");
         String content = StringUtils.join(FileUtils.readLines(resource.getFile(), "UTF-8").toArray(), "\n");
         stubFor(get(urlPathEqualTo(url)).willReturn(aResponse().withBody(content).withStatus(200)));
     }
