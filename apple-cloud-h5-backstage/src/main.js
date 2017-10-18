e8a885f5ce03db5fc64a038d12afd9e41cf434c3
tangmin <191102902@qq.com>
2017-10-18 12:50:28 +0800

    app端第三方登录,注册

--- apple-cloud-h5-backstage/src/main.js
+++ apple-cloud-h5-backstage/src/main.js
@@ -1,18 +1,18 @@
 import 'babel-polyfill'
 import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import fastclick from 'fastclick'
 
 import 'common/stylus/index.styl'
 
-Vue.use(ElementUI)
-Vue.config.productionTip = false
-fastclick.attach(document.body)
+Vue.use(Element, { size: 'mini' });
+Vue.config.productionTip = false;
+fastclick.attach(document.body);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
